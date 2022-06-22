23b189427462302045d779d02055129f9ddb1a57
pankona <yosuke.akatsuka@gmail.com>
2022-06-22 13:55:48 +0900

    ちょっと違うので一回削除

--- main.go
+++ main.go
@@ -1,41 +1,4 @@
 package main
 
-import "fmt"
-
-var route = map[string][]string{
-	"a": {"b", "c", "d"},
-	"b": {"e", "f"},
-	"c": {"e", "f", "g"},
-	"d": {"f", "g"},
-	"e": {"h", "i"},
-	"f": {"h", "i", "j"},
-	"g": {"i", "j"},
-	"h": {"k", "l"},
-	"i": {"k", "l", "m"},
-	"j": {"l", "m"},
-	"k": {"n"},
-	"l": {"n"},
-	"m": {"n"},
-	"n": {},
-}
-
 func main() {
-	d, err := ConstructDungeon(route, "a")
-	if err != nil {
-		fmt.Printf("failed to construct dungeon: %v", err)
-	}
-
-	// 最初から最後までとりあえず出力してみる
-	current := d.Start()
-	for !current.IsDeadEnd() {
-		fmt.Println(current)
-		current = current.next[0]
-	}
-	fmt.Println(current)
-
-	// 途中から再開するときはこのような
-	fmt.Println(d.GetByName("i"))
-
-	r := &SampleRoom{}
-	r.Show(r.InitialState())
 }
