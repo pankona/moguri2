6064de747c5d5399effbba2ea634cdfb5281936a
pankona <yosuke.akatsuka@gmail.com>
2022-07-23 21:04:07 +0900

    work as http server

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/pankona/moguri2/moguri"
 )
 
@@ -14,31 +18,67 @@ func main() {
 		StateStore: &globalStateStore{
 			state: map[string]moguri.State{
 				characterID: &globalState{
-					currentInteraction: &choiceRoomInteraction{},
+					currentInteraction: NewChoiceRoomInteraction(),
 				},
 			},
 		},
 	}
 
-	ctx := context.Background()
+	r := mux.NewRouter()
 
-	i, err := m.GetCurrentInteraction(ctx, characterID)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(i.GetCurrentMessage())
-	fmt.Println(i.GetCurrentChoices())
+	r.Handle("/ping", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		resp := struct {
+			Pong string `json:"pong"`
+		}{
+			Pong: "Hello!",
+		}
 
-	actionNum := 0
-	err = m.Interact(ctx, characterID, actionNum)
-	if err != nil {
-		panic(err)
-	}
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			log.Printf("error: %v", err)
+		}
+	})).Methods(http.MethodGet)
 
-	i, err = m.GetCurrentInteraction(ctx, characterID)
-	if err != nil {
-		panic(err)
+	r.Handle("/current_interaction", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		i, err := m.GetCurrentInteraction(r.Context(), characterID)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("error: %v", err), http.StatusInternalServerError)
+			return
+		}
+
+		resp := struct {
+			Message string   `json:"message"`
+			Choices []string `json:"choices"`
+		}{
+			Message: i.GetCurrentMessage(),
+			Choices: i.GetCurrentChoices(),
+		}
+
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			log.Printf("error: %v", err)
+		}
+	})).Methods(http.MethodGet)
+
+	r.Handle("/interact", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		req := struct {
+			ActionNum int `json:"action_num"`
+		}{}
+
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, fmt.Sprintf("error: %v", err), http.StatusInternalServerError)
+			return
+		}
+
+		if err := m.Interact(r.Context(), characterID, req.ActionNum); err != nil {
+			http.Error(w, fmt.Sprintf("error: %v", err), http.StatusInternalServerError)
+			return
+		}
+	})).Methods(http.MethodPost)
+
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		if !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("error occurred: %v", err)
+		}
 	}
-	fmt.Println(i.GetCurrentMessage())
-	fmt.Println(i.GetCurrentChoices())
 }
